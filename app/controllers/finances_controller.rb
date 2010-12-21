0a18c114b1b482f66a2f7e6943e5315cfc1e0660
Adam Bachman <adam.bachman@gmail.com>
2010-12-21 14:37:34 -0500

    updated finance views

--- app/controllers/finances_controller.rb
+++ app/controllers/finances_controller.rb
@@ -1,6 +1,10 @@
 class FinancesController < ApplicationController
   def index
     authorize! :manage, Finance
+
+    @members = User.order :username
+    @invoices = Invoice.where(['paid IS NULL OR paid = ?', false])
+    @payment = Payment.new
   end
 
 end
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,5 +1,5 @@
 class Invoice < ActiveRecord::Base
-  belongs_to :membership
+  belongs_to :membership, :include => :user
   has_one :payment
 
   DUES = "dues"
@@ -11,5 +11,13 @@ class Invoice < ActiveRecord::Base
     [DUES, WORKSHOP, DONATION, BEVERAGE_FUND].map do |reason|
       [reason.titleize, reason]
     end
+  end
+
+  def self.unpaid
+    where(['paid IS NULL OR paid = ?', false])
+  end
+
+  def self.by_username
+    joins({:membership => :user}).order('users.username')
   end
 end
