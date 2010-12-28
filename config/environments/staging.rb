46899afa63baf2c3d0d70274c6af2bb65ccb347a
Adam Bachman <adam.bachman@gmail.com>
2010-12-28 13:56:39 -0500

    added default host

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -47,4 +47,6 @@ Nodewise::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.default_url_options = { :host => "nodewise-staging.heroku.com"}
 end
