c04cb7f88df39d2d7deb05112087ad9dbaa5158c
Adam Bachman <adam.bachman@gmail.com>
2010-12-28 14:27:48 -0500

    updated production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,5 +47,6 @@ Nodewise::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => "nodewise-staging.heroku.com"}
+  # CHANGE THIS TO YOUR LOCAL HOSTNAME
+  config.action_mailer.default_url_options = { :host => ENV['SENDGRID_DOMAIN']}
 end
